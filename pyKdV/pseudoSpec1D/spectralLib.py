894f727b1661973f2fc032ed6cfc85e274b2eced
Martin Deshaies-Jacques <martn@martn.info>
2013-09-10 12:01:33 -0400

    kdvTestGrad.f95 was missing
    also minor addition (unused) to
    ./pyKdV/pseudoSpec1D/spectralLib.py

--- pyKdV/pseudoSpec1D/spectralLib.py
+++ pyKdV/pseudoSpec1D/spectralLib.py
@@ -1,6 +1,26 @@
 import numpy as np
 import numpy.fft as fft
 
+
+class spectralLibError(Exception):
+    pass
+#----------------------------------------------------------
+
+def conjT(M):
+    return M.conjugate().T
+
+#----------------------------------------------------------
+
+def fft2d(M):
+    return conjT(np.fft.fft(conjT(np.fft.fft(M))))    
+
+
+def ifft2d(M):
+    #return g.N*np.fft.ifft2(M)
+    return conjT(np.fft.ifft(conjT(np.fft.ifft(M)))) 
+
+#----------------------------------------------------------
+
 def fftOrder(grid):
     n=np.zeros(grid.N)
     for i in xrange(grid.N):
@@ -56,4 +76,12 @@ def specFilt(f,grid,Ntrc=None):
     
 #----------------------------------------------------------
 
-
+def specTrunc(w, grid, Ntrc=None):
+    if Ntrc==None:
+        Ntrc=grid.Ntrc
+    n=fftOrder(grid)
+    for i in xrange(grid.N):
+        # truncature
+        if np.abs(n[i])>Ntrc:
+            w[i]=0.
+    return w
---
+++ src/kdvTestGrad.f95
@@ -0,0 +1,77 @@
+program kdvTestGrad
+
+use kdvProp
+use kdvTLMProp
+use kdvTLMTest
+implicit none
+
+integer                 ::  N, Ntrc, nDt, maxPower, i
+double precision        ::  L, pAmp, diff, dt, tReal
+logical                 ::  test
+
+double precision, dimension(:), allocatable ::  alph, beta, gamm, rho, &
+                                                ic, forc
+double precision, dimension(:), allocatable  :: xBuff, yBuff
+
+
+integer, dimension(5)    ::  nDtVec
+double precision, dimension(:, :), allocatable  ::  u
+
+Ntrc=50
+N=3*Ntrc+1
+L=3.D2
+
+
+allocate(xBuff(N), yBuff(N))
+allocate(ic(N), alph(N), beta(N), gamm(N), rho(N), forc(N))
+
+pAmp=1.D-1
+dt=1.D-2
+
+
+maxPower=-9
+nDtVec=(/2,10,20,50,100/)
+
+! Generating random fields
+!   unfiltered state vectors
+ic=initRandVec(N)
+xBuff=pAmp*initRandVec(N)
+yBuff=pAmp*initRandVec(N)
+!   filtered parameters vectors
+alph=initRandVec(N, Ntrc)
+beta=initRandVec(N, Ntrc)
+gamm=initRandVec(N, Ntrc)
+rho=initRandVec(N, Ntrc)
+forc=pAmp*initRandVec(N, Ntrc)
+
+
+
+do i=1,5
+    nDt=nDtVec(i)
+    print *, '============================================================='
+    print *, '============================================================='
+    print *, 'nDt=',nDt
+    allocate(u(nDt+1, N))
+    u=kdvPropagator(N, Ntrc, L, dt, nDt, tReal, ic, &
+                            alph, beta, gamm, rho, forc)
+
+    print *, '============================================================='
+    print *, 'Testing adjoint validity of kdvTLMPropagator'
+    if (testKdvTLMPropagatorAdj(N, Ntrc, L, dt, nDt, pAmp, diff, &
+                        u, xBuff, yBuff, &
+                        alph, beta, gamm, rho)) then 
+        print *, ' >>Test succeeded:', diff
+    else
+        print *, ' >>Test FAILED', diff
+        test=.false.
+    end if
+
+    print *, 
+    print *, '============================================================='
+    print *, 'Gradient test'
+    call testGradient(N, Ntrc, L, dt, nDt, maxPower,&
+                                        u, xBuff, &
+                                        alph, beta, gamm, rho)
+    deallocate(u)
+end do
+end program kdvTestGrad

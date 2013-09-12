01ca32b86201986d8f8fe18710a0c9b43cb51aaf
Martin Deshaies-Jacques <martn@martn.info>
2013-09-12 12:33:53 -0400

    integrateNDt et adjointNDt: in terms of nDt
    instead of tInt (float).  The integer division
    will introduces a roundup that could result in
    adjoint incompatibility.

--- pyKdV/kdvTLMLauncher.py
+++ pyKdV/kdvTLMLauncher.py
@@ -75,17 +75,27 @@ class TLMLauncher(object):
         nDt=int((tInt)/dt)
         nDt0=int(t0/dt)
 
+        return  self.integrateNDt(nDt, nDt0, fullPertTraj=fullPertTraj)
+   
+    #-----------------------------------
+
+    def integrateNDt(self, nDt, nDt0, fullPertTraj=False):
+        self.nDt=nDt
+        self.nDt0=nDt0
+
         if fullPertTraj:
             self.fullPertTraj=True
             self.pertTraj=Trajectory(self.grid)
             self.pertTraj.initialize(self.pert, tInt, dt)
+        else:
+            self.fullPertTraj=False
 
-        return self.__kdvTLMProp_Fortran(dt, nDt, nDt0, 
-                                            fullPertTraj=fullPertTraj)
-    
+        return self.__kdvTLMProp_Fortran(self.refTraj.dt, nDt, nDt0, 
+                                            fullOutput=self.fullPertTraj)
+
     #-------------------------------------------------------
 
-    def adjoint(self, tInt=None, t0=0., resetTReal=True):         
+    def adjoint(self, tInt=None, t0=0.):         
 
         if tInt==None : tInt=self.refTraj.tInt
 
@@ -97,8 +107,13 @@ class TLMLauncher(object):
         nDt=int((tInt)/dt)
         nDt0=int(t0/dt)
         
-        return self.__kdvTLMProp_Fortran_Adj(dt, nDt, nDt0) 
+        return self.adjointNDt(nDt, nDt0)
 
+    #-----------------------------------
+    def adjointNDt(self, nDt, nDt0):
+        self.nDt=nDt
+        self.nDt0=nDt0
+        return self.__kdvTLMProp_Fortran_Adj(self.refTraj.dt, nDt, nDt0) 
     
     #-------------------------------------------------------
 
@@ -146,13 +161,13 @@ class TLMLauncher(object):
     #------------------------------------------------------
 
     def __kdvTLMProp_Fortran(
-            self, dt, nDt, nDt0=0, fullPertTraj=True):
+            self, dt, nDt, nDt0=0, fullOutput=True):
 
         # Local variables names
         grid=self.grid
         param=self.param
 
-        if fullPertTraj:
+        if fullOutput:
             self.pertTraj.putData(fKdV.fKdVTLMPropagator(
                                 grid.N, grid.Ntrc, grid.L, dt, nDt,
                                 self.pert, 
@@ -178,8 +193,7 @@ class TLMLauncher(object):
 
     #------------------------------------------------------
 
-    def __kdvTLMProp_Fortran_Adj(
-            self, dt, nDt, nDt0=0, fullPertTraj=True):
+    def __kdvTLMProp_Fortran_Adj(self, dt, nDt, nDt0=0):
 
         # Local variables names
         grid=self.grid
@@ -227,7 +241,6 @@ if __name__=='__main__':
     import matplotlib.pyplot as plt
     from kdvLauncher import *
     from kdvMisc import *
-    import random as rnd
 
     grid=SpectralGrid(150,300.)
     tInt=3.
@@ -251,6 +264,52 @@ if __name__=='__main__':
     LAdj_x=dxLauncher.adjoint()
     print(np.dot(dx, Ldy)-np.dot(LAdj_x, dy))
 
+    #----| partial trajectory time |----
     Ldy=dyLauncher.integrate(tInt/3., tInt/2.)
     LAdj_x=dxLauncher.adjoint(tInt/3., tInt/2.)
     print(np.dot(dx, Ldy)-np.dot(LAdj_x, dy))
+
+
+    #----| step integrations |----------
+    step=tInt/3.
+    L1=TLMLauncher(param, u, dy)
+    L1dy=L1.integrate(step, t0=0.)
+
+    L2=TLMLauncher(param, u, L1dy)
+    L2L1dy=L2.integrate(step, t0=L1.tReal)
+    
+    A2=TLMLauncher(param, u, L2L1dy)
+    A2dx=A2.adjoint(step, t0=L1.tReal)
+
+    A1=TLMLauncher(param, u, A2dx)
+    A1A2dx=A1.adjoint(step, t0=0.)
+    print(np.dot(dx, L2L1dy)-np.dot(A1A2dx, dy))
+
+    #----| step integrations |----------
+    step=tInt/3.
+    L1=TLMLauncher(param, u, dy)
+    L1dy=L1.integrate(step, t0=0.)
+    nDt_1=L1.nDt
+    nDt0_1=L1.nDt0
+    print(nDt0_1, nDt_1)
+    
+    L2=TLMLauncher(param, u, L1dy)
+    L2L1dy=L2.integrate(step, t0=L1.tReal)
+    nDt_2=L2.nDt
+    nDt0_2=L2.nDt0
+    print(nDt0_2, nDt_2)
+
+    A2=TLMLauncher(param, u, L2L1dy)
+    A2dx=A2.adjointNDt(nDt_2, nDt0_2)
+    A1=TLMLauncher(param, u, A2dx)
+    A1A2dx=A2.adjointNDt(nDt_1, nDt0_1)
+
+    A1=TLMLauncher(param, u, dx)
+    A1dx=dxLauncher.adjointNDt(nDt_1, nDt0_1)
+    print("<dx,L1dy>-<A1dx, dy>     = %+.15g"%(np.dot(dx, L1dy)\
+                                            -np.dot(A1dx, dy)))
+    print("<dx,L2L1dy>-<A2dx, L1dy> = %+.15g"%(np.dot(dx, L2L1dy)\
+                                            -np.dot(A2dx, L1dy)))
+    print("<dx,L2L1dy>-<A1A2dx, dy> = %+.15g"%(np.dot(dx, L2L1dy)\
+                                            -np.dot(A1A2dx, dy)))
+

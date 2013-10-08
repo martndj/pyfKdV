b2777b96557866786ab2d60e4e1bf624918b853f
Martin Deshaies-Jacques <martn@martn.info>
2013-10-08 13:48:33 -0400

    time dependant parameters

--- pyKdV/kdvParam.py
+++ pyKdV/kdvParam.py
@@ -16,12 +16,6 @@ class Param(object):
         In the later case, function must be of one variable
         (the space variable 'x')
 
-        <TODO>  For now parameters (including forcing) are static
-                although localized (Param.py)
-                It would be interresting for parameters to be
-                Trajectory object, although, the Fortran model
-                would have to be profundly modified.
-
         <TODO>  parameters defined as array  
 
     """
@@ -34,51 +28,148 @@ class Param(object):
 
         if not (isinstance(grid, PeriodicGrid)):
             raise self.ParamError("grid must be an instance of PeriodicGrid")
-
         self.grid=grid
-        self.forcing=self.__setFunc(forcing)
-        self.alpha=self.__setFunc(alpha)
-        self.beta=self.__setFunc(beta)
-        self.gamma=self.__setFunc(gamma)
-        self.rho=self.__setFunc(rho)
+        
+        if (isinstance(forcing, Trajectory) 
+            or isinstance(alpha, Trajectory) 
+            or isinstance(beta, Trajectory)
+            or isinstance(gamma, Trajectory)
+            or isinstance(rho, Trajectory)):
+            self.__initTraj(forcing, alpha, beta, gamma, rho)
 
-        self.shape=(5,grid.N)
+            self.isTrajectory=True
+            self.shape=(self.nDtParam, 5,grid.N)
+
+        else:
+            self.forcing=self.__setFunc(forcing)
+            self.alpha=self.__setFunc(alpha)
+            self.beta=self.__setFunc(beta)
+            self.gamma=self.__setFunc(gamma)
+            self.rho=self.__setFunc(rho)
+            
+            self.isTrajectory=False
+            self.shape=(5,grid.N)
 
     #----------------------------------------------------------------
 
-    def __setFunc(self, fct):
+    def __initTraj(self, forcing, alpha, beta, gamma, rho):
+        params=(forcing, alpha, beta, gamma, rho)
+        nDtParam=0
+        issetDt=False
+        for p in params:
+            if isinstance(p, Trajectory):
+                if not issetDt:
+                    self.dt=p.dt
+                    issetDt=True
+                else:
+                    if p.dt<>self.dt:
+                        raise ParamError(
+                        "parameter trajectories must have the same dt")
+
+                if p.nDt>nDtParam:
+                    nDtParam=p.nDt
+            self.nDtParam=nDtParam
+
+        self.forcing=self.__setTraj(forcing)
+        self.alpha=self.__setTraj(alpha)
+        self.beta=self.__setTraj(beta)
+        self.gamma=self.__setTraj(gamma)
+        self.rho=self.__setTraj(rho)
+            
+
+    #----------------------------------------------------------------
+
+    def __setTraj(self, param):
+
+        if isinstance(param, Trajectory):
+            if param.nDt<self.nDtParam:
+                data=params[i].getData()
+                newdata=np.empty(shape=(self.nDtParam+1, self.grid.N))
+                newdata[0:params[i].nDt+1,:]=data
+                newdata[params[i].nDt+1:, :]=data[params[i].nDt, :]
+                traj=Trajectory(self.grid)
+                traj.initialize(param[0], self.nDtParam, self.dt)
+                traj.putData(newdata)
+                 
+            if param.nDt==self.nDtParam:
+                traj=param
+    
+        elif param==None:
+            traj=Trajectory(self.grid)
+            traj.initialize(np.zeros(self.grid.N), self.nDtParam, self.dt)
+            traj.putData(np.zeros(shape=(self.nDtParam+1, self.grid.N)\
+                            *param))
+        elif isinstance(param, (int, float)):
+            traj=Trajectory(self.grid)
+            traj.initialize(np.zeros(self.grid.N), self.nDtParam, self.dt)
+            traj.putData(np.ones(shape=(self.nDtParam+1, self.grid.N)\
+                            *param))
+
+        elif callable(param):
+            constData=np.vectorize(param)(self.grid.x)
+            data=np.empty(shape=(self.nDtParam+1, self.grid.N))
+            data[:]=constData
+            traj=Trajectory(self.grid)
+            traj.initialize(np.zeros(self.grid.N), self.nDtParam, self.dt)
+            traj.putData(data)
+
+        else:
+            raise self.ParamError("<None|float|Trajectory>")
+        
+        traj.incrmTReal(finished=True, tReal=self.nDtParam*self.dt)
+        return traj
+            
+    #----------------------------------------------------------------
+
+    def __setFunc(self, param):
         def wrapper(c):
             def funcConst(x):
                 return float(c)
             return funcConst
         
-        if fct==None:
+        if param==None:
             return wrapper(0.)
-        elif isinstance(fct, (int, float)):
-            return wrapper(fct)
-        elif callable(fct):
-            return fct
+        elif isinstance(param, (int, float)):
+            return wrapper(param)
+        elif callable(param):
+            return param
         else:
-            raise self.ParamError("<None|function>")
+            raise self.ParamError("<None|float|function>")
             
 
 
     #----------------------------------------------------------------
 
     def __getitem__(self,i):
-        if i==0:
-            return np.vectorize(self.forcing)(self.grid.x)
-        elif i==1:
-            return np.vectorize(self.alpha)(self.grid.x)
-        elif i==2:
-            return np.vectorize(self.beta)(self.grid.x)
-        elif i==3:
-            return np.vectorize(self.gamma)(self.grid.x)
-        elif i==4:
-            return np.vectorize(self.rho)(self.grid.x)
+        if self.isTrajectory:
+            if i==0:
+                return self.forcing
+            elif i==1:
+                return self.alpha
+            elif i==2:
+                return self.beta
+            elif i==3:
+                return self.gamma
+            elif i==4:
+                return self.rho
+            else:
+                raise IndexError('[0:forcing(x,t), 1:alpha(x,t),'+
+                                '2:beta(x,t), 3:gamma(x,t), 4:rho(x,t)]')
         else:
-            raise IndexError('[0:forcing(x), 1:alpha(x),'+
-                            '2:beta(x), 3:gamma(x), 4:rho(x)]')
+            if i==0:
+                return np.vectorize(self.forcing)(self.grid.x)
+            elif i==1:
+                return np.vectorize(self.alpha)(self.grid.x)
+            elif i==2:
+                return np.vectorize(self.beta)(self.grid.x)
+            elif i==3:
+                return np.vectorize(self.gamma)(self.grid.x)
+            elif i==4:
+                return np.vectorize(self.rho)(self.grid.x)
+            else:
+                raise IndexError('[0:forcing(x), 1:alpha(x),'+
+                                '2:beta(x), 3:gamma(x), 4:rho(x)]')
+        
 
     #----------------------------------------------------------------
 
@@ -115,7 +206,23 @@ if __name__=='__main__':
     def func3(x):
         return np.sin(x)
 
-    p=Param(grid, func2, func2, func1, func1, func3)
-    plt.plot(grid.x, p[4])
+    def funcTimeDependant(x, t):
+        return np.sin(x-t)*np.cos(t)
+    nDtParam=100
+    dt=0.1
+    data=np.zeros(shape=(nDtParam+1, grid.N))
+    for n in xrange(nDtParam+1):
+        data[n,:]=funcTimeDependant(grid.x, n*dt)
+    traj=Trajectory(grid)
+    traj.initialize(data[0,:], nDtParam, dt)
+    traj.putData(data)
+
+    p=Param(grid, traj, func2, func1, func1, func3)
+    if p.isTrajectory:
+        p[1].waterfall()
+    else:
+        plt.plot(grid.x, p[4])
     
     plt.show() 
+
+

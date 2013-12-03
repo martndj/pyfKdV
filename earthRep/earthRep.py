5d661d53440e8459df7cb2434984e47b8980ee41
Martin Deshaies-Jacques <martn@martn.info>
2013-12-02 21:38:03 -0500

    modified:   earthRep.py

--- earthRep/earthRep.py
+++ earthRep/earthRep.py
@@ -1,41 +1,46 @@
-from mpl_toolkits.basemap import Basemap, maskoceans, interp
+from mpl_toolkits.basemap import Basemap
 import matplotlib.cm as cm
 import numpy as np
-from pseudoSpec1D import PeriodicGrid, bumpSchwartz
+from pseudoSpec1D import PeriodicGrid
 #-----------------------------------------------------------
 
-def earthRepresentation(v, grid,  
-        lon_0=-100, lat_0=65, resDpi=200, projection='ortho', 
-        resolution='l', jetLat=45., jetWidth=10., amplMax=10.,
-        bluemarble=True):
+def grid2Sphere(v, grid, m, lon_0=-107, jetLat=50., amplMax=5.):
+    nLons=grid.N
+    lons = np.linspace(-180.+lon_0, 180+lon_0, nLons)
+    lats = np.ones(nLons)*jetLat +v*amplMax
+    x,y= m(lons, lats)
+    return x,y
+
+    
+
+def ortho(v, grid,  
+        lon_0=-107, lat_0=50, resDpi=200, projection='ortho', 
+        resolution='l', jetLat=45., amplMax=5., coord=True, **kwargs):
 
     if not isinstance(grid, PeriodicGrid):
         raise TypeError()
 
-    bump=np.vectorize(bumpSchwartz)
-
     m = Basemap(projection=projection,lon_0=lon_0,lat_0=lat_0,
                 resolution=resolution)
 
+    x,y=grid2Sphere(v, grid, m, lon_0=lon_0, jetLat=45., amplMax=amplMax)
 
-    # make up some data on a regular lat/lon grid.
-    nLons=grid.N
-    nLats=nLons/2
-    delta = 2.*np.pi/(nLons-1)
-
-    lats = (0.5*np.pi-delta*np.indices((nLats,nLons))[0,:,:])
-    lons = (delta*np.indices((nLats,nLons))[1,:,:])
-    x, y = m(lons*180./np.pi, lats*180./np.pi)
-
-    jetCoord=np.array([jetLat, jetWidth])/180.*np.pi 
+    m.plot(x,y, **kwargs)
+    if coord:
+        m.drawmeridian(np.linspace(0,350, 18))
+        m.drawparallel(np.linspace(-80,80, 8))
+    return m 
 
 
-    pert=v/grid.norm(v, metric=np.infty)*amplMax/180.*np.pi
 
-    jetCoreMod=jetCoord[0]+pert
-    longWave = bump((lats-jetCoreMod)/jetCoord[1])
+def overCanada(v, grid, amplMax=5., **kwargs):
+    m = Basemap(width=12000000,height=9000000,projection='lcc',
+            resolution='c',lat_1=45.,lat_2=55,lat_0=50,lon_0=-107.)
 
+    if not isinstance(grid, PeriodicGrid):
+        raise TypeError()
 
-    m.contour(x,y,longWave,15,linewidths=1.5, cmap=cm.autumn)
-    if bluemarble : m.bluemarble()
+    x,y=grid2Sphere(v, grid, m, lon_0=-107, jetLat=45., amplMax=amplMax)
 
+    m.plot(x, y, **kwargs)
+    return m
